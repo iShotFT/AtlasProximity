f0ff229d2f0bc86e3158ab4d1ee47e00ac11be10
vagrant <vagrant@atlasproximity>
2019-01-23 10:49:57 +0000

    Initial commit for first deployment.

---
+++ deploy.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Project name
+set('application', 'AtlasProximity');
+
+// Project repository
+set('repository', 'https://github.com/iShotFT/AtlasProximity.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true);
+
+// Shared files/dirs between deploys 
+add('shared_files', [
+    '.env',
+]);
+
+add('shared_dirs', [
+    'storage',
+]);
+
+// Writable dirs by web server 
+add('writable_dirs', [
+    'bootstrap/cache',
+    'storage',
+    'storage/app',
+    'storage/app/public',
+    'storage/framework',
+    'storage/framework/cache',
+    'storage/framework/sessions',
+    'storage/framework/views',
+    'storage/logs',
+]);
+set('allow_anonymous_stats', false);
+
+// Hosts
+host('46.101.104.25')->user('deployer')->identityFile('~/.ssh/deployerkey')->set('deploy_path', '/var/www/atlas.heraldofdeath.com');
+
+// Tasks
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+task('reload:php-fpm', function () {
+    run('sudo /usr/sbin/service php7.2-fpm reload');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+// Migrate database before symlink new release.
+
+before('deploy:symlink', 'artisan:migrate');
+
+after('deploy', 'reload:php-fpm');
+after('rollback', 'reload:php-fpm');
+
