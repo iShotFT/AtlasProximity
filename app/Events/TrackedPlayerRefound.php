43d8d8f4389d36fc3115cb2544461cfaebca3f00
Andries Verbanck <averbanck1992@gmail.com>
2019-01-26 14:35:14 +0100

    Offline / online messages for tracked players

---
+++ app/Events/TrackedPlayerRefound.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Events;
+
+use App\PlayerTrack;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Queue\SerializesModels;
+
+class TrackedPlayerRefound implements ShouldBroadcastNow
+{
+    use SerializesModels;
+
+    protected $player;
+    protected $last;
+    protected $last_seen;
+    protected $guildid;
+    protected $channelid;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(PlayerTrack $playerTrack)
+    {
+        $this->player = $playerTrack->player;
+        $this->last   = $playerTrack->last_coordinate;
+
+        $this->guildid   = $playerTrack->guild_id;
+        $this->channelid = $playerTrack->channel_id;
+    }
+
+    public function broadcastWith()
+    {
+        return [
+            'player'    => $this->player,
+            'last'      => $this->last,
+            'guildid'   => $this->guildid,
+            'channelid' => $this->channelid,
+        ];
+    }
+
+    public function broadcastAs()
+    {
+        return 'tracked.player.refound';
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel
+     */
+    public function broadcastOn()
+    {
+        return new Channel('public');
+    }
+}
