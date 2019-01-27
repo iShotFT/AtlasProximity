9b3de3a76650c263877d729dd6b2cf5b1fd7d15f
Andries Verbanck <averbanck1992@gmail.com>
2019-01-27 09:30:39 +0100

    Set deploy to notify on the Discord when an update happens (webhook)

--- deploy.php
+++ deploy.php
@@ -3,13 +3,66 @@
 namespace Deployer;
 
 require 'recipe/laravel.php';
-require 'recipe/discord.php';
-//require 'recipe/npm.php';
 
+use Deployer\Task\Context;
+use Deployer\Utility\Httpie;
+
+set('discord_webhook', function () {
+    return 'https://discordapp.com/api/webhooks/{{discord_channel}}/{{discord_token}}/slack';
+});
+// Deploy messages
+set('discord_notify_text', function () {
+    return [
+        'text' => parse(':information_source: **{{user}}** is deploying branch `{{branch}}` to _{{target}}_'),
+    ];
+});
+set('discord_success_text', function () {
+    return [
+        'text' => parse(':white_check_mark: Branch `{{branch}}` deployed to _{{target}}_ successfully'),
+    ];
+});
+set('discord_failure_text', function () {
+    return [
+        'text' => parse(':no_entry_sign: Branch `{{branch}}` has failed to deploy to _{{target}}_'),
+    ];
+});
+// The message
+set('discord_message', 'discord_notify_text');
+// Helpers
+task('discord_send_message', function () {
+    $message = get(get('discord_message'));
+    Httpie::post(get('discord_webhook'))->body($message)->send();
+});
+// Tasks
+desc('Just notify your Discord channel with all messages, without deploying');
+task('discord:test', function () {
+    set('discord_message', 'discord_notify_text');
+    invoke('discord_send_message');
+    set('discord_message', 'discord_success_text');
+    invoke('discord_send_message');
+    set('discord_message', 'discord_failure_text');
+    invoke('discord_send_message');
+})->once()->shallow();
+desc('Notify Discord');
+task('discord:notify', function () {
+    set('discord_message', 'discord_notify_text');
+    invoke('discord_send_message');
+})->once()->shallow()->isPrivate();
+desc('Notify Discord about deploy finish');
+task('discord:notify:success', function () {
+    set('discord_message', 'discord_success_text');
+    invoke('discord_send_message');
+})->once()->shallow()->isPrivate();
+desc('Notify Discord about deploy failure');
+task('discord:notify:failure', function () {
+    set('discord_message', 'discord_failure_text');
+    invoke('discord_send_message');
+})->once()->shallow()->isPrivate();
 // Discord
 //https://discordapp.com/api/webhooks/538996091576778753/4VMHrmqrBzsbSRr7_Hqdlfg6gFhQixFb50eQPC3frp4-7eiaek77Gyp3IuOgQpID7QW_
 set('discord_channel', '538996091576778753');
 set('discord_token', '4VMHrmqrBzsbSRr7_Hqdlfg6gFhQixFb50eQPC3frp4-7eiaek77Gyp3IuOgQpID7QW_');
+set('user', 'iShot');
 
 // Project name
 set('application', 'AtlasProximity');
