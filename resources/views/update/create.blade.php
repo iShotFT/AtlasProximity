e36be99b6eacbb0ee080cad5d0e365df06b1b7ff
Andries Verbanck <averbanck1992@gmail.com>
2019-01-27 15:13:41 +0100

    Fix for creating an update when none exist yet

--- resources/views/update/create.blade.php
+++ resources/views/update/create.blade.php
@@ -13,18 +13,18 @@
                             @method('POST')
                             <div class="form-group">
                                 <label for="version">Version</label>
-                                <input type="number" class="form-control" id="version" name="version" aria-describedby="versionHelp" placeholder="Version" value="{{ $previous_update->version }}">
-                                <small id="versionHelp" class="form-text text-muted">Previous version: {{ $previous_update->version }}</small>
+                                <input type="number" class="form-control" id="version" name="version" aria-describedby="versionHelp" placeholder="Version" value="{{ $previous_update ? $previous_update->version : '' }}">
+                                <small id="versionHelp" class="form-text text-muted">Previous version: {{ $previous_update ? $previous_update->version : 'none' }}</small>
                             </div>
                             <div class="form-group">
                                 <label for="major">Major</label>
-                                <input type="number" class="form-control" id="major" name="major" aria-describedby="majorHelp" placeholder="Major" value="{{ $previous_update->major }}">
-                                <small id="majorHelp" class="form-text text-muted">Previous major: {{ $previous_update->major }}</small>
+                                <input type="number" class="form-control" id="major" name="major" aria-describedby="majorHelp" placeholder="Major" value="{{ $previous_update ? $previous_update->major : '' }}">
+                                <small id="majorHelp" class="form-text text-muted">Previous major: {{ $previous_update ? $previous_update->major : 'none' }}</small>
                             </div>
                             <div class="form-group">
                                 <label for="minor">Minor</label>
-                                <input type="number" class="form-control" id="minor" name="minor" aria-describedby="minorHelp" placeholder="Minor" value="{{ $previous_update->minor + 1 }}">
-                                <small id="minorHelp" class="form-text text-muted">Previous minor: {{ $previous_update->minor }}</small>
+                                <input type="number" class="form-control" id="minor" name="minor" aria-describedby="minorHelp" placeholder="Minor" value="{{ $previous_update ? $previous_update->minor + 1 : '' }}">
+                                <small id="minorHelp" class="form-text text-muted">Previous minor: {{ $previous_update ? $previous_update->minor : 'none' }}</small>
                             </div>
                             <div class="form-group">
                                 <label for="changes">Changes</label>
