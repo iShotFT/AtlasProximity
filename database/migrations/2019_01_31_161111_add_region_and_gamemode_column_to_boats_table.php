795c7464a0255b6f8c5b874d1b48248ce6fbb144
Andries Verbanck <andries@digitalmind.be>
2019-01-31 17:11:56 +0100

    Add region gamemode columns to boats table

---
+++ database/migrations/2019_01_31_161111_add_region_and_gamemode_column_to_boats_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddRegionAndGamemodeColumnToBoatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('boats', function (Blueprint $table) {
+            $table->string('gamemode', 3)->default('pvp')->after('name');
+            $table->string('region', 2)->default('eu')->after('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('boats', function (Blueprint $table) {
+            //
+            $table->dropColumn([
+                'gamemode',
+                'region',
+            ]);
+        });
+    }
+}
