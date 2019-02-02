e1ab34b6fa14bbb90998a41d9b8c6a17d6e4abc3
Andries Verbanck <averbanck1992@gmail.com>
2019-02-02 14:04:56 +0100

    Get rid of required login / register on /get

--- resources/views/underconstruction.blade.php
+++ resources/views/underconstruction.blade.php
@@ -4,5 +4,6 @@
     <div class="d-flex justify-content-center align-items-center flex-column">
         <h1>Coming soon</h1>
         <a href="{{ url('/docs') }}" class="btn btn-success">Get the bot installed using our documentation page</a>
+        <a href="https://discord.gg/KMHkqtb" class="btn btn-info">Join our Discord for info & updates</a>
     </div>
 @stop
